6a7ea003fa4ccc909b4369f17c09fe1e015b9daf
pasca-l <shion.yamadate@keio.jp>
2023-01-01 18:21:07 +0900

    added chakraui

--- src/components/molecules/coin_list.tsx
+++ src/components/molecules/coin_list.tsx
@@ -1,30 +1,64 @@
-import { Card, CardHeader, CardBody, CardFooter, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Checkbox,
+  Stack,
+  StackDivider,
+  SimpleGrid,
+} from "@chakra-ui/react";
+
+import { coin_data } from "../../public/coin_data";
 
 type CoinType = {
-  id: number;
+  uid: string;
+  url: string;
+  year: string;
+  country: string;
   name: string;
-  isCollected: boolean;
+  isCollected: number;
 };
 
-type Props = CoinType[];
-
-const coins = [
-  { id: 0, name: "jflakse", isCollected: false },
-  { id: 1, name: "asjflkj", isCollected: false },
-];
+// type Props = CoinType[];
 
 export const CoinList = () => {
   return (
-    <>
-      {coins.map(({ id, name }: CoinType) => {
+    <SimpleGrid minChildWidth="320px" spacing="20px">
+      {coin_data.map(({ uid, name, isCollected }: CoinType) => {
         return (
-          <Card key={id}>
+          <Card key={uid}>
             <CardBody>
-              <Text>{name}</Text>
+              <Stack
+                direction={"row"}
+                spacing="20px"
+                divider={<StackDivider />}
+              >
+                <Image
+                  src={"/images/" + uid + ".jpg"}
+                  width={150}
+                  height={150}
+                  alt={name}
+                />
+                <Stack direction={"column"} spacing="20px">
+                  <Text>{name}</Text>
+                  {!!isCollected ? (
+                    <Checkbox defaultChecked></Checkbox>
+                  ) : (
+                    <Checkbox></Checkbox>
+                  )}
+                </Stack>
+              </Stack>
             </CardBody>
           </Card>
         );
       })}
-    </>
+    </SimpleGrid>
   );
 };
+
+export async function getStaticProps() {
+  return { propos: { coin_data } };
+}
