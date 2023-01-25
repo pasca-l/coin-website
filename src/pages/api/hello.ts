8369a936e66a5236d2c0ee7fff8c7796dea09923
pasca-l <shion.yamadate@keio.jp>
2023-01-25 20:16:15 +0900

    added fastapi

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // if (req.method == "POST") {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/");
+  const users = await response.json();
+  res.status(200).json({ users });
+  // } else {
+  //   res.status(200).json({ name: "John Doe" });
+  // }
 }
